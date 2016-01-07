4bc25c56344975bf0fafe296368bfc2d8527d88e
Sebastien Rannou <mxs@sbrk.org>
2016-01-07 23:01:32 +0100

    Added distribution of parts.

--- galette.js
+++ galette.js
@@ -47,6 +47,15 @@ $(document).ready(function () {
 	});
     }
 
+    var bind_distrib = function () {
+	switch_cursor();
+	$(".for-me").click(function () {
+	    $(this).parent().append('<img width="150" height="150" class="part" src="' + $("#cursor").attr('src') + '" />');
+	    $(this).remove();
+	    switch_cursor();
+	});
+    }
+
     var bind_table = function () {
 	var assiettes = ["http://www.elle-mosaique.com/428-1181-thickbox/assiette-plate-digoin-modele-cibon.jpg",
 			 "http://www.designers-avenue.com/5272-large_default/grand-cru-soft-assiette-27-cm-4-pcs-rosendahl.jpg",
@@ -54,12 +63,13 @@ $(document).ready(function () {
 			 "http://www.cristallerie-de-paris.fr/image/article/boite_de_6_assiettes_creuses_2338.jpg"];
 
 	var copains = "";
-	console.log(names);
 	for (var i = 0; i < names.length; ++i) {
 	    var copain = "";
 	    copain += "<div class='copain'>";
 	    copain += "<h4>Assiette de <b>" + names[i] + "</b></h4>";
 	    copain += "<img width='200' height='200' src='" + assiettes[Math.floor(Math.random() * assiettes.length)] + "' />";
+	    copain += '<br />';
+	    copain += "<input class='for-me' type='submit' value='Ici!' class='big' />";
 	    copain += "</div>";
 
 	    copains += copain;
@@ -68,7 +78,22 @@ $(document).ready(function () {
 	var current = $("#table").html();
 	current = current.replace('{copains}', copains);
 	$("#table").html(current);
-	switch_page("#table");
+	switch_page("#table") && bind_distrib();
+    }
+
+    var switch_cursor = function () {
+	var parts = ["http://www.bioentet.fr/wp-content/uploads/2011/01/part_galette.gif",
+		     "http://www.atome77.com/images/articles/part-galette-2011.raw.jpg",
+		     "http://www.asmaacuisine.com/wp-content/uploads/2010/01/imgp6027.jpg",
+		     "http://www.lesepicesrient.fr/wp-content/uploads/2010/12/galette-rois-tonka.jpg"];
+
+	$("#cursor").remove();
+	var cursor = "";
+	cursor += "<img style='position: absolute' id='cursor' width='200' height='200' src='" + parts[Math.floor(Math.random() * parts.length)] + "' />";
+	$("#page").append(cursor);
+	$("#page").mousemove(function(e){
+	    $('#cursor').css({'top': e.clientY + 50, 'left': e.clientX + 50});
+	});
     }
 
     switch_page("#home") && bind_home();
